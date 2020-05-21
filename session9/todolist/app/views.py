e46335d43e803b864c5f96dc785e8e73b3c7713b
Younghyun <bebeseo1@naver.com>
2020-05-21 18:47:26 +0900

    원격 레포 연결!

---
+++ session9/todolist/app/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from .models import Post
+
+# Create your views here.
+def home(request):
+    posts = Post.objects.order_by('duedate')
+    return render(request, 'home.html', {'posts' : posts})
+
+def detail(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    return render(request, 'detail.html', {'post' : post})
+
+def new(request):
+    if request.method == "POST":
+        new_post = Post.objects.create(
+            title = request.POST['title'],
+            content = request.POST['content'],
+            duedate = request.POST['duedate']
+        )
+        return redirect('detail', new_post.pk)
+    else:
+        return render(request, 'new.html')
+
+def edit(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    if request.method == "POST":
+        Post.objects.filter(pk=post_pk).update(
+            title = request.POST['title'],
+            content = request.POST['content'],
+            duedate = request.POST['duedate']
+        )
+        return redirect('detail', post_pk)
+    else:
+        return render(request, 'edit.html', {'post' : post})
+
+def delete(request, post_pk):
+    Post.objects.get(pk=post_pk).delete()
+    return redirect('home')
